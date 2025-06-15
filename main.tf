701a5abb0038dd447e17bc4355105e0b5e6660ab
Thomas Bell <thomasjbell065@gmail.com>
2025-06-15 13:23:20 -0400

    used variables

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
-resource random_integer rint {
-  min = 1
-  max = 600
+
+
+resource "local_file" "sample_res" {
+  filename = var.filename
+  content = var.content
 }
 
-output "resultint" {
-  value = random_integer.rint.result
-}
 
-resource "random_string" "rstring" {
-  length           = 16
-}
+# resource random_integer rint {
+#   min = 1
+#   max = 600
+# }
 
-output "resultstring" {
-  value = random_string.rstring.result
-}
+# output "resultint" {
+#   value = random_integer.rint.result
+# }
+
+# resource "random_string" "rstring" {
+#   length           = 16
+# }
+
+# output "resultstring" {
+#   value = random_string.rstring.result
+# }
 
 # resource "local_file" "cat_res" {
 #   filename = "cat.txt"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+  type = string
+  default = "sample.txt"
+}
+
+variable "content" {
+  type = string
+  default = "I Love coding with Terraform"
+}
