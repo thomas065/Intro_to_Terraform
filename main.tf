0e6de64e60e6fd6f6eb634780ee933cbb6a702de
Thomas Bell <thomasjbell065@gmail.com>
2025-06-15 12:32:01 -0400

    added multiple resources

--- main.tf
+++ main.tf
@@ -3,8 +3,18 @@
 #   content  = "I love Terraform"
 # }
 
-resource "local_sensitive_file" "sample_res2" {
-  filename = "sample_args.txt"
-  content = "I love Terraform"
-  file_permission = "0700"
+# resource "local_sensitive_file" "sample_res2" {
+#   filename = "sample_args.txt"
+#   content = "I love Terraform"
+#   file_permission = "0700"
+# }
+
+resource "local_file" "cat_res" {
+  filename = "cat.txt"
+  content  = "I love Cats"
+}
+
+resource "local_file" "dog_res" {
+  filename = "dog.txt"
+  content  = "I love Dogs"
 }
