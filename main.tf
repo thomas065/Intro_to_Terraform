c15dffebf0b7ff6dc11c675d756c47f1a84e01a4
Thomas Bell <thomasjbell065@gmail.com>
2025-06-15 13:03:25 -0400

    used random strings and integers

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
-# resource "local_file" "sample_res" {
-#   filename = "sample.txt"
-#   content  = "I love Terraform"
-# }
-
-# resource "local_sensitive_file" "sample_res2" {
-#   filename = "sample_args.txt"
-#   content = "I love Terraform"
-#   file_permission = "0700"
-# }
-
-resource "local_file" "cat_res" {
-  filename = "cat.txt"
-  content  = "I love Cats"
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
+resource random_integer rint {
+  min = 1
+  max = 600
 }
 
-resource "local_file" "dog_res" {
-  filename = "dog.txt"
-  content  = "I love Dogs"
+output "resultint" {
+  value = random_integer.rint.result
+}
+
+resource "random_string" "rstring" {
+  length           = 16
+}
+
+output "resultstring" {
+  value = random_string.rstring.result
 }
