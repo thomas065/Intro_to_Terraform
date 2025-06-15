5468b022dc1eee3a5050d6b2511dd7c6fcf8b5a5
Thomas Bell <thomasjbell065@gmail.com>
2025-06-15 14:33:05 -0400

    using explicit and implicit variables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Intro_to_Terraform
 
 -   Following along with Udemy course
-    > > Sections 1 - 9 by 06/17/25
+    > Sections 1 - 9 by 06/17/25
--- main.tf
+++ main.tf
@@ -1,18 +1,37 @@
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/integer
 
 
-resource "local_file" "sample_res" {
-  filename = var.filename
-  content = var.content6.age
+resource "local_file" "random_res1" {
+  filename = "explicit.txt"
+  content = "I love random strings of text:🤯 ${random_string.rand_name1.id}"
+  depends_on = [ random_string.rand_name1 ]
 }
 
-output "result_filename" {
-  value = local_file.sample_res.filename
+resource "random_string" "rand_name1" {
+  length           = 20
 }
 
-output "result_content" {
-  value = local_file.sample_res.content
+resource "local_file" "random_res2" {
+  filename = "implicit.txt"
+  content = "I love random strings of text:🤯 ${random_string.rand_name2.id}"
 }
+
+resource "random_string" "rand_name2" {
+  length           = 10
+}
+
+# resource "local_file" "sample_res" {
+#   filename = var.filename
+#   content = var.content6.age
+# }
+
+# output "result_filename" {
+#   value = local_file.sample_res.filename
+# }
+
+# output "result_content" {
+#   value = local_file.sample_res.content
+# }
 
 # resource "local_file" "sample_res" {
 #   filename = var.filename
